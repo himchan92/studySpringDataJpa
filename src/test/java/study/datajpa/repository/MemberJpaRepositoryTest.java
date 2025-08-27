505a2f7fc74e750c293df95378418da2a4b5e7c1
himchan92 <omea92@naver.com>
2025-08-27 12:12:06 +0900

    Member Team JPA CRUD 복습

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -17,6 +17,9 @@ class MemberJpaRepositoryTest {
     @Autowired
     private MemberJpaRepository memberJpaRepository;
 
+    @Autowired
+    private MemberRepository memberRepository;
+
     @Test
     public void basicCRUD() {
         Member member1 = new Member("member1");
@@ -46,6 +49,38 @@ class MemberJpaRepositoryTest {
 
         //지워서 비었으니 0이면 성공
         long deleteCount = memberJpaRepository.count();
+        assertThat(deleteCount).isEqualTo(0);
+    }
+
+    @Test
+    public void basicCRUD2() {
+        Member member1 = new Member("member1");
+        Member member2 = new Member("member2");
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        member1.setUsername("member1!!!!"); //변경감지 UPDATE 수행
+
+        //카운트 2건등록했으니 2맞으면 성공
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        //지워서 비었으니 0이면 성공
+        long deleteCount = memberRepository.count();
         assertThat(deleteCount).isEqualTo(0);
     }
 }
