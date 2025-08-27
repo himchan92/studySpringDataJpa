75f0d40d85b18a8bac19de316887a1799b64ca46
himchan92 <omea92@naver.com>
2025-08-27 13:27:59 +0900

    spring data jpa custom 복습

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -1,9 +1,13 @@
 package study.datajpa.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.dto.MemberDTO;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -17,5 +21,14 @@ public class MemberController {
     public String findMember(@PathVariable("id") Long id) {
         Member member = memberRepository.findById(id).get();
         return member.getUsername();
+    }
+
+    //페이징 Spring Data Jpa 지원
+    //json 결과 API 스펙맞게 특정필드만 노출시키기위해 별도 DTO 객체로 반환(실무 거의 필수)
+    //페이징은 1아닌 0부터 시작한다!!
+    @GetMapping("/members")
+    public Page<MemberDTO> list(@PageableDefault(size = 5) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(MemberDTO::new);
     }
 }
