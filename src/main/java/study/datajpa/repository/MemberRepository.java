e30b44d02dacccc17425b587e605167d20bb22bb
himchan92 <omea92@naver.com>
2025-08-27 12:18:24 +0900

    Member Team JPA CRUD 복습

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -3,5 +3,11 @@ package study.datajpa.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    //별도 작성없이 findBy필드명 규칙으로 JPA에서 제공
+    //List<Member> findByUsername(String username);
+
 }
