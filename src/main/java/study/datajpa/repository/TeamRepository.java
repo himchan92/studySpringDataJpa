004b5954a58183d176b2bdfbe8c93055b2524644
himchan92 <omea92@naver.com>
2025-12-22 11:57:59 +0900

    review init

--- src/main/java/study/datajpa/repository/TeamRepository.java
+++
@@ -1,8 +0,0 @@
-package study.datajpa.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import study.datajpa.entity.Team;
-
-// 컴포넌트 스캔되어있는 JpaRespository 상속받아서 별도 Repository 어노테이션 명시 생략가능
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
