f437f3bdcd0e711b397483aea3b98e6b6214622d
himchan92 <omea92@naver.com>
2025-12-23 12:17:41 +0900

    등록일/수정일 공통 jpa 실습

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -8,7 +8,7 @@ import lombok.*;
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @ToString(of = {"id", "username", "age", "team"})
-public class Member {
+public class Member extends BaseEntity {
 
     @Id
     @GeneratedValue
