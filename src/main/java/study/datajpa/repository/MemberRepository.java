8757c14b83b9da568c57aea6f871a42041e52ef9
himchan92 <omea92@naver.com>
2025-12-23 12:09:09 +0900

    커스텀 레파지토리 실습

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -7,13 +7,14 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import java.util.List;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
     @Query("select m from Member m where m.username = : username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
 
-    @Query("select new study.datajpa.dto(m.id, m.username, t.name) from Member m join m.team t")
+    @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
+            "from Member m join m.team t")
     List<MemberDto> findMemberDto();
 }
--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -42,4 +42,9 @@ class MemberRepositoryTest {
         assertThat(result.get(0).getAge()).isEqualTo(20);
         assertThat(result.size()).isEqualTo(1);
     }
+
+    @Test
+    public void callCustom() {
+        List<Member> result = memberRepository.findMemberCustom();
+    }
 }
